773c56499e9edde03b385351167af06f1e50a255
Raknarrok <raknarrok@hotmail.com>
2023-09-24 21:04:57 -0600

    Adding Only Delviery Stuffs
    Old data was removed and the delivery info was keeped.

---
+++ deliveryserver/ProductManager.js
@@ -0,0 +1,262 @@
+/*
+Go to cd deliveryserver
+Run node app.js
+*/
+const fs = require('fs')
+
+class ProductManager {
+  constructor (filePath) {
+    this.productIdCounter = 1
+    this.filePath = filePath
+    this.products = this.checkFile()
+  }
+
+  addProduct ({ title, description, price, thumbnail, code, stock }) {
+    // First Validate if we have all the required fields
+    if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
+      console.error('Hey!!! You are missing one or more required fields. Please provide values for title, description, price, thumbnail, code, and stock to continue.')
+      return
+    }
+
+    // Read the file and parse the content to an array
+    const fileContent = this.checkFile()
+
+    // Verify if the file is empty.
+    if (fileContent) {
+      // Verify if the code is already in use
+      const isCodeInUse = this.products.some((product) => product.code === code)
+      if (isCodeInUse) {
+        // throw new Error(`The code ${code} is already in use Code must be unique.`)
+        return `The code ${code} is already in use Code must be unique.`
+      }
+
+      const product = {
+        id: this.productIdCounter,
+        title,
+        description,
+        price,
+        thumbnail,
+        code: code || `P${this.productIdCounter}`,
+        stock
+      }
+      this.products.push(product)
+      this.saveFile()
+      this.productIdCounter++
+    } else {
+      // If the file is empty, create an array with the first product
+      const product = {
+        id: this.productIdCounter,
+        title,
+        description,
+        price,
+        thumbnail,
+        code: code || `P${this.productIdCounter}`,
+        stock
+      }
+      this.products.push(product)
+      this.saveFile()
+      this.productIdCounter++
+    }
+  }
+
+  removeProductByCode (code) {
+    const index = this.products.findIndex((product) => product.code === code)
+    if (index !== -1) {
+      this.products.splice(index, 1)
+      this.saveFile()
+    } else {
+      throw new Error(`The code ${code} doesnt exist, please verify and try again.`)
+    }
+  }
+
+  removeProductById (id) {
+    const index = this.products.findIndex((product) => product.id === id)
+    if (index !== -1) {
+      this.products.splice(index, 1)
+      this.saveFile()
+    } else {
+      throw new Error(`The id ${id} doesnt exist, please verify and try again.`)
+    }
+  }
+
+  removeProduct (product) {
+    this.removeProductByCode(product.code)
+  }
+
+  getProductByCode (code) {
+    const product = this.products.find((product) => product.code === code)
+    if (!product) {
+      return 'Not Found'
+    }
+    return product
+  }
+
+  getProductById (id) {
+    const product = this.products.find((product) => product.id === id)
+    if (!product) {
+      return 0
+    }
+    return product
+  }
+
+  updateProductByCode (code, updatedProduct) {
+    const index = this.products.findIndex((product) => product.code === code)
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedProduct }
+      this.saveFile()
+    }
+  }
+
+  updateProductById (id, updatedProduct) {
+    const index = this.products.findIndex((product) => product.id === id)
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedProduct }
+      this.saveFile()
+    }
+  }
+
+  updateProductList (newProductList) {
+    this.products = newProductList
+  }
+
+  getAllProducts () {
+    return this.products
+  }
+
+  getProducts (limit) {
+    if (!limit) {
+      return this.products
+    } else {
+      return this.products.slice(0, limit)
+    }
+  }
+
+  checkFile () {
+    try {
+      // Read the file and parse the content to an array
+      const fileContent = fs.readFileSync(this.filePath, 'utf8')
+      // Verify if the file is empty.
+      if (!fileContent) {
+        return []
+      }
+      return JSON.parse(fileContent) || []
+    } catch (error) {
+      console.error(error)
+      return []
+    }
+  }
+
+  saveFile () {
+    fs.writeFileSync(this.filePath, JSON.stringify(this.products, null, 2))
+  }
+}
+
+const listElements = new ProductManager('./products.txt')
+
+// Products Creation Section
+const productOne = {
+  title: 'Collar Slim',
+  description: '½ pulgada de ancho Nombre y 1 numero de tel bordado',
+  price: 100,
+  thumbnail: 'https://raknarrok.github.io/static/images/productos/collares/slim.png',
+  code: 'CXS',
+  stock: 10
+}
+
+const productTwo = {
+  title: 'Collar Normal',
+  description: '1 pulgada de ancho Nombre y 1 numero de tel bordado',
+  price: 200,
+  thumbnail: 'https://raknarrok.github.io/static/images/productos/collares/normal.png',
+  code: 'CN',
+  stock: 10
+}
+
+const productThree = {
+  title: 'Collar Ancho',
+  description: '1 ½ pulgada de ancho Nombre y 1 numero de tel bordado',
+  price: 200,
+  thumbnail: 'https://raknarrok.github.io/static/images/productos/collares/ancho.png',
+  code: 'CA',
+  stock: 30
+}
+
+const productFour = {
+  title: 'Collar Especial',
+  description: 'Diferentes anchos, colores y diseños. Nombre y 1 numero de tel bordado',
+  price: 450,
+  thumbnail: 'https://raknarrok.github.io/static/images/productos/collares/ancho.png',
+  code: 'CE',
+  stock: 5
+}
+
+const productFive = {
+  title: 'Plaquita Hueso S',
+  description: 'Plaquita de hueso de 1.5 cm de ancho por 1 cm de alto. Nombre y 1 numero de tel bordado',
+  price: 450,
+  thumbnail: 'https://raknarrok.github.io/static/images/productos/collares/ancho.png',
+  code: 'PHS',
+  stock: 5
+}
+
+const productSix = {
+  title: 'Dog Tag Type 1',
+  description: 'This is a dog tag type 1',
+  price: 9.99,
+  thumbnail: 'https://example.com/dogtag1.jpg',
+  code: 'DT1',
+  stock: 15
+}
+
+const productSeven = {
+  title: 'Dog Tag Type 2',
+  description: 'This is a dog tag type 2',
+  price: 12.99,
+  thumbnail: 'https://example.com/dogtag2.jpg',
+  code: 'DT2',
+  stock: 8
+}
+
+const productEight = {
+  title: 'Dog Tag Type 3',
+  description: 'This is a dog tag type 3',
+  price: 14.99,
+  thumbnail: 'https://example.com/dogtag3.jpg',
+  code: 'DT3',
+  stock: 3
+}
+
+const productNine = {
+  title: 'Dog Tag Type 4',
+  description: 'This is a dog tag type 4',
+  price: 8.99,
+  thumbnail: 'https://example.com/dogtag4.jpg',
+  code: 'DT4',
+  stock: 20
+}
+
+const productTen = {
+  title: 'Dog Tag Type 5',
+  description: 'This is a dog tag type 5',
+  price: 11.99,
+  thumbnail: 'https://example.com/dogtag5.jpg',
+  code: 'DT5',
+  stock: 12
+}
+
+// Verify list is empty
+console.log('------Empty Array Is displayed', listElements.getAllProducts())
+
+// Adding products
+listElements.addProduct(productOne)
+listElements.addProduct(productTwo)
+listElements.addProduct(productThree)
+listElements.addProduct(productFour)
+listElements.addProduct(productFive)
+listElements.addProduct(productSix)
+listElements.addProduct(productSeven)
+listElements.addProduct(productEight)
+listElements.addProduct(productNine)
+listElements.addProduct(productTen)
+
+module.exports = ProductManager
---
+++ deliveryserver/app.js
@@ -0,0 +1,48 @@
+/*
+Go to cd deliveryserver
+Run node app.js
+*/
+const express = require('express')
+const app = express()
+const PORT = 8080
+const ProductManager = require('./ProductManager')
+const productManager = new ProductManager('./products.txt')
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await productManager.getProducts(req.query.limit)
+    if (products.length === 0) {
+      res.status(404).json({ error: 'No hay productos cargados' })
+    } else {
+      res.json({ products })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.get('/api/products/:pdi', async (req, res) => {
+  const pdi = parseInt(req.params.pdi)
+  try {
+    const products = await productManager.getProductById(pdi)
+    if (products === 0) {
+      res.status(404).json({ error: 'Item Not Found' })
+    } else {
+      res.json({ products })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+server.on('error', (error) => {
+  console.log(`Error: ${error}`)
+})
