a427c11813bcb83f421cfb644576f4b2c038cb58
cristianjovanovich <cristian.j@revelaciondata.com.ar>
2023-10-20 02:55:14 -0300

    Add socket channel to create product and refresh table

--- mipekicha/src/app.js
+++ mipekicha/src/app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import handlerbars from 'express-handlebars'
 import __dirname from './utils.js'
-import viewsRouter from './routes/views.router.js'
-import productRouter from './routes/product.routes.js'
+import viewsRoutes from './routes/views.routes.js'
+import productsRoutes from './routes/product.routes.js'
 import ProductManager from './controllers/ProductManager.js'
 import { Server } from 'socket.io'
 
@@ -11,45 +11,44 @@ const PORT = 8080
 const productManager = new ProductManager('./products.txt')
 
 app.use(express.json())
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended: true }))
 
 // Handlebars configuration
 app.engine('handlebars', handlerbars.engine())
-app.set('view engine','handlebars') // Extencion of these files
-app.set('views', __dirname+'/views') // Indicate the folder where the files are
+app.set('view engine', 'handlebars') // Extencion of these files
+app.set('views', __dirname + '/views') // Indicate the folder where the files are
 
 // Static files configuration
-app.use(express.static(__dirname+'public'))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', async (req,res)=>{
-    const allProducts = await productManager.getAllProducts()
-    res.render('home', {
-      products: allProducts
-    })
+// Usamos las rutas importadas
+app.use('/', viewsRoutes)
+app.use('/api/products', productsRoutes)
+
+const httpServer = app.listen(PORT, () => {
+  console.log('Servidor activo en puerto ' + PORT)
 })
 
-app.post('/products', async (req,res)=>{
-  const newProduct = req.body
-  productManager.addProduct(newProduct)
-  res.redirect('/')
-})
-
-app.get('/api/product/:productId', async (req,res)=>{
-    // const prod = await productManager.getProductById()
-    const productId = parseInt(req.params.productId)
-    const product = await productManager.getProductById(productId)
-    res.render('product', {
-      products: product
-    })
-})
-
-// app.use('/', )
-
-const httpServer = app.listen(PORT, ()=>{
-    console.log('Servidor activo en puerto ' + PORT)
-})
+//Socket
 const io = new Server(httpServer)
+io.on('connection', socket => {
+  console.log('Cliente conectado')
+  socket.on('disconnect', () => {
+    console.log('Un usuario se desconectó')
+  })
 
-io.on('connection', (socket) => {
-  console.log('new client')
+  // Aca capturamos el emit que hicimos en el frontend, algo asi como que
+  // "escuchamos" ese evento
+  socket.on('add-product', data => {
+    // Agregamos el producto enviado desde el frontend
+    const newProduct = productManager.addProduct(data)
+
+    // Emitimos un evento, esta vez desde el backend y enviamos el producto nuevo que se creo
+    io.emit('product-added', newProduct)
+  })
+
+  socket.on('delete-product', productId => {
+    productManager.deleteProduct(productId)
+    io.emit('product-deleted', productId)
+  })
 })
